6edc048fd1b712cb7f3cfc9a0d3f0c2b3239fd89
JuniYadi <juniyadi94@gmail.com>
2020-07-26 14:25:24 +0700

    Update Index

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <>
-      <h1>Index</h1>
+      <h1 className="text-green-800">Index</h1>
 
       <p><Link href="/about"><a>About</a></Link></p>
       <p><Link href="/product/kvm-vps"><a>KVM VPS</a></Link></p>
