18a5a57185c79ea8729c53242249f95b30eb8c86
JuniYadi <juniyadi94@gmail.com>
2020-07-27 15:54:36 +0700

    Added Layouts Components

---
+++ components/Layouts.jsx
@@ -0,0 +1,11 @@
+import Header from './Header'
+import Navbar from './Navbar'
+
+export default function Layouts() {
+    return (
+        <>
+            <Navbar />
+            <Header />
+        </>
+    )
+}
