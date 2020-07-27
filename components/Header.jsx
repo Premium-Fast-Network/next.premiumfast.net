3900331c1242e8594128ca2a457b3c52099f899c
JuniYadi <juniyadi94@gmail.com>
2020-07-27 20:08:53 +0700

    Update Component Header: Register Button

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,7 +14,8 @@ export default function Header() {
                     <h1 className="my-4 text-5xl font-bold leading-tight">Selamat datang.</h1>
                     <p className="leading-normal text-2xl mb-8">Premium Fast Network menyediakan berbagai produk seperti RDP User, KVM VPS, VPN, Seedbox, Whatsapp Gateway dan masih banyak lagi. Jangan ragu untuk chat, jika punya pertanyaan tentang produk kami.</p>
 
-                    <button onClick={onClickRegister} className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">Register</button>
+                    <button onClick={onClickRegister} className="mx-auto lg:mx-0 hover:bg-green-700 hover:text-white bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">Register</button>
+
                 </div>
                 <div className="w-full md:w-3/5 py-6 text-right">
                     <img className="w-full md:w-4/5 z-50" src="/img/undraw_business_shop.svg" />
