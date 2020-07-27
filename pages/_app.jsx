1632e6da9d156f97a8a7f935a3be136b366d1f51
JuniYadi <juniyadi94@gmail.com>
2020-07-27 21:03:40 +0700

    Added FontAwesome to NextJS SSR

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,12 @@
 import '../styles/app.css';
 
+// FontAwesome in SSR (Server Side Rendering)
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
+
+// Added FontAwesome CSS
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
