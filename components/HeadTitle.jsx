621d09c1c4146e7b9a655d3acf43bd0f3c3610be
JuniYadi <juniyadi94@gmail.com>
2020-07-27 23:55:51 +0700

    Update HeadTitle Component: Added More Meta Tags

--- components/HeadTitle.jsx
+++ components/HeadTitle.jsx
@@ -2,10 +2,17 @@ import Head from 'next/head'
 import { Website } from '../config/Website'
 
 export default function HeadTitle(props) {
+    const customHead = props.customHead
+
     return (
         <>
             <Head>
-                <title>{props.customHead.title} - {Website.name}</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+                <title>{customHead.title} - {Website.name}</title>
+                <meta name="description" content={customHead.description ?? ''} />
+                <meta name="keywords" content={customHead.keywords ?? ''} />
+                <meta name="author" content={Website.name} />
             </Head>
         </>
     )
