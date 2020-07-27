ebba6011ae59911346142e550c6c69f4097be7ed
JuniYadi <juniyadi94@gmail.com>
2020-07-27 16:44:39 +0700

    Added Component WhyUs

---
+++ components/Whyus.jsx
@@ -0,0 +1,131 @@
+export default function Whyus() {
+    return (
+        <>
+            <section className="bg-white border-b py-8">
+                <div className="container max-w-5xl mx-auto m-8">
+                    <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+                        Kenapa Harus PremiumFast<span className="text-red-700">.NET</span> ?
+                    </h1>
+                    <div className="w-full mb-4">
+                        <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+                    </div>
+
+                    <div className="flex flex-wrap flex-col-reverse sm:flex-row">
+                        <div className="w-full sm:w-1/2 p-6 mt-6">
+                            <img className="w-full sm:h-40 mx-auto" src="/img/undraw_appreciation.svg"
+                                alt="Terpercaya" />
+                        </div>
+                        <div className="w-full sm:w-1/2 p-6 mt-6">
+                            <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
+                                Terpercaya
+                            </h3>
+                            <p className="text-gray-600 mb-8">
+                                Kami telah hadir sejak 2013 dan hingga sekarang tetap menjadi mitra para pelanggan kami dan akan tetap konsisten untuk memberikan pelayanan terbaik.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-wrap">
+                        <div className="w-5/6 sm:w-1/2 p-6">
+                            <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
+                                Pembayaran Automatis
+                            </h3>
+                            <p className="text-gray-600 mb-8">
+                                Kami mendukung pembayaran automatis melalui: <br />
+                                1. QRIS (QR Code) dan OVO <br />
+                                2. Credit Card<br />
+                                3. BCA KlikPay<br />
+                                4. Retail (Alfamart/Pos Indonesia/Pegadaian)<br />
+                                5. Virtual Account (BRI/BNI/Mandiri)
+                            </p>
+                        </div>
+                        <div className="w-full sm:w-1/2 p-6">
+                            <img className="w-full sm:h-40 mx-auto" src="/img/undraw_online_payments.svg"
+                                alt="Online Payments" />
+                        </div>
+                    </div>
+
+                    <div class="flex flex-wrap flex-col-reverse sm:flex-row">
+                        <div class="w-full sm:w-1/2 p-6 mt-6">
+                            <img class="w-full sm:h-40 mx-auto" src="/img/undraw_outer_space.svg" alt="Instant Activation" />
+                        </div>
+                        <div class="w-full sm:w-1/2 p-6 mt-6">
+                            <div class="align-middle">
+                                <h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">
+                                    Instant Activation
+                                </h3>
+                                <p class="text-gray-600 mb-8">Beberapa produk kami seperti RDP User/KVM VPS/VPN/Leechbox, Akan
+                                    aktif secara automatis setelah anda melakukan atau melunasi pembayaran sesuai invoice
+                                    tagihan.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-wrap">
+                        <div class="w-5/6 sm:w-1/2 p-6">
+                            <h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">
+                                Email/Whatsapp Notification
+                            </h3>
+                            <p class="text-gray-600 mb-8">
+                                Kami akan mengirimkan notifikasi via email dan whatsapp kepada anda seperti tentang pembelian
+                                baru, perpanjangan produk, support tiket atau pengumuman tentang produk yang anda gunakan.
+                            </p>
+                        </div>
+                        <div class="w-full sm:w-1/2 p-6">
+                            <img class="w-full sm:h-40 mx-auto" src="/img/undraw_notify_re.svg"
+                                alt="Email/Whatsapp Notification" />
+                        </div>
+                    </div>
+
+                    <div class="flex flex-wrap flex-col-reverse sm:flex-row">
+                        <div class="w-full sm:w-1/2 p-6 mt-6">
+                            <img class="w-full sm:h-40 mx-auto" src="/img/undraw_note_list_re.svg" alt="Client Area" />
+                        </div>
+                        <div class="w-full sm:w-1/2 p-6 mt-6">
+                            <div class="align-middle">
+                                <h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">
+                                    Client Area
+                                </h3>
+                                <p class="text-gray-600 mb-8">
+                                    Kami menyediakan Client Area untuk mempermudah anda mengecek tentang status order produk
+                                    anda, tagihan, support tiket dan pengumuman terbaru dari kami.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-wrap">
+                        <div class="w-5/6 sm:w-1/2 p-6">
+                            <h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">
+                                Support
+                            </h3>
+                            <p class="text-gray-600 mb-8">
+                                Jika anda mempunyai masalah pada produk yang anda gunakan, jangan ragu untuk menghubungi support kami melalui support tiket yang bisa anda buka 24 jam. Staff kami akan segera merespon tiket anda ketika online.
+                            </p>
+                        </div>
+                        <div class="w-full sm:w-1/2 p-6">
+                            <img class="w-full sm:h-40 mx-auto" src="/img/undraw_questions.svg"
+                                alt="Support" />
+                        </div>
+                    </div>
+
+                    <div class="flex flex-wrap flex-col-reverse sm:flex-row">
+                        <div class="w-full sm:w-1/2 p-6 mt-6">
+                            <img class="w-full sm:h-40 mx-auto" src="/img/undraw_connected_world.svg" alt="Multiple Server" />
+                        </div>
+                        <div class="w-full sm:w-1/2 p-6 mt-6">
+                            <div class="align-middle">
+                                <h3 class="text-3xl text-gray-800 font-bold leading-none mb-3">
+                                    Multiple Server
+                                </h3>
+                                <p class="text-gray-600 mb-8">
+                                    Kami menawarkan berbagai lokasi server dari berbagai negara untuk menyesuaikan kebutuhan anda dari berbagai paket yang kami tawarkan.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
