8a57099eb8c0cd7a007699d342e4df2e0ff9941f
JuniYadi <juniyadi94@gmail.com>
2020-07-28 02:25:25 +0700

    Update Website Configuration

--- config/Website.jsx
+++ config/Website.jsx
@@ -1,4 +1,7 @@
 export const Website = {
     name: 'Premium Fast Network',
-    clientarea: 'https://manage.premiumfast.net'
+    clientarea: 'https://manage.premiumfast.net',
+    email: 'cs@premiumfast.net',
+    phone: '+62857 999-02074',
+    whatsapp: '6285799902074',
 }
