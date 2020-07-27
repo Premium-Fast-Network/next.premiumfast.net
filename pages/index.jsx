0d3b4a94ccff1a07d9062c1d38210dbf8af2989c
JuniYadi <juniyadi94@gmail.com>
2020-07-27 23:55:31 +0700

    Update Page Index: Head Data and Login Button

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,20 +3,27 @@ import Whyus from '../components/Whyus'
 import HeadContent from '../components/HeadContent'
 import HeadTitle from '../components/HeadTitle'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faUserEdit, faSignInAlt } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home() {
 
   // Head Data
   const head = {
     title: 'Home',
-    description: '',
+    description: 'Premium Fast Network menyediakan berbagai produk seperti RDP User, KVM VPS, VPN, Seedbox, Whatsapp Gateway dan masih banyak lagi. Jangan ragu untuk chat, jika punya pertanyaan tentang produk kami.',
+    keywords: 'RDP, RDP User, KVM, KVM VPS, KVM VPS Singapore, KVM VPS Canada, KVM VPS France, Whatsapp Gateway, VPN'
   }
 
   function onClickRegister(e) {
     e.preventDefault();
 
     window.location.href = "https://manage.premiumfast.net/register.php"
+  }
+
+  function onClickLogin(e) {
+    e.preventDefault();
+
+    window.location.href = "https://manage.premiumfast.net/login.php"
   }
 
   return (
@@ -27,10 +34,17 @@ export default function Home() {
         <HeadContent>
           <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
             <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
-                <h1 className="my-4 text-4xl font-bold leading-tight"><FontAwesomeIcon icon={faHome} /> Selamat datang.</h1>
-                <p className="leading-normal text-2xl mb-8">Premium Fast Network menyediakan berbagai produk seperti RDP User, KVM VPS, VPN, Seedbox, Whatsapp Gateway dan masih banyak lagi. Jangan ragu untuk chat, jika punya pertanyaan tentang produk kami.</p>
+              <h1 className="my-4 text-4xl font-bold leading-tight"><FontAwesomeIcon icon={faHome} /> Selamat datang.</h1>
+              <p className="leading-normal text-2xl mb-8">Premium Fast Network menyediakan berbagai produk seperti RDP User, KVM VPS, VPN, Seedbox, Whatsapp Gateway dan masih banyak lagi. Jangan ragu untuk chat, jika punya pertanyaan tentang produk kami.</p>
 
-                <button onClick={onClickRegister} className="mx-auto lg:mx-0 hover:bg-green-700 hover:text-white bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">Register</button>
+              <div>
+                <button nClick={onClickRegister} className="hover:bg-green-800 hover:text-white bg-white text-gray-800 font-bold rounded py-2 px-4 rounded-full mx-2 my-2">
+                  <FontAwesomeIcon icon={faUserEdit} /> Register
+                </button>
+                <button nClick={onClickRegister} className="hover:bg-green-800 hover:text-white bg-white text-gray-800 font-bold rounded py-2 px-4 rounded-full mx-2 my-2">
+                  <FontAwesomeIcon icon={faSignInAlt} /> Login
+                </button>
+              </div>
 
             </div>
             <div className="w-full md:w-3/5 py-6 text-right">
