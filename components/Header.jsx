67199468b7f358bada31d8fbf79b25659f5f9341
JuniYadi <juniyadi94@gmail.com>
2020-07-27 20:33:31 +0700

    Update Components Header: Added FontAwesome in Head

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+
 export default function Header() {
 
     function onClickRegister(e) {
@@ -11,7 +14,7 @@ export default function Header() {
             <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
                 <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
                     <p className="uppercase tracking-loose w-full">Hi.. Apa kabar?</p>
-                    <h1 className="my-4 text-5xl font-bold leading-tight">Selamat datang.</h1>
+                    <h1 className="my-4 text-4xl font-bold leading-tight"><FontAwesomeIcon icon={faHome} /> Selamat datang.</h1>
                     <p className="leading-normal text-2xl mb-8">Premium Fast Network menyediakan berbagai produk seperti RDP User, KVM VPS, VPN, Seedbox, Whatsapp Gateway dan masih banyak lagi. Jangan ragu untuk chat, jika punya pertanyaan tentang produk kami.</p>
 
                     <button onClick={onClickRegister} className="mx-auto lg:mx-0 hover:bg-green-700 hover:text-white bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">Register</button>
