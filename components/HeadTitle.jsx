41158abbc3797da5968aa60f049b3fb57dba55ee
JuniYadi <juniyadi94@gmail.com>
2020-07-28 09:21:38 +0700

    Update HeadTitle Components With Route

--- components/HeadTitle.jsx
+++ components/HeadTitle.jsx
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import { Website } from '../config/Website'
+import { withRouter } from 'next/router'
 
-export default function HeadTitle(props) {
+function HeadTitle(props) {
     const customHead = props.customHead
+
+    // get now location
+    const location = props.router
 
     return (
         <>
@@ -13,7 +17,18 @@ export default function HeadTitle(props) {
                 <meta name="description" content={customHead.description ?? ''} />
                 <meta name="keywords" content={customHead.keywords ?? ''} />
                 <meta name="author" content={Website.name} />
+                <meta property="og:url" content={location.route} />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content={`${customHead.title} - ${Website.name}`} />
+                <meta property="og:description" content={customHead.description ?? ''} />
+                <meta property="og:image" content={customHead.image ?? ''} />
+                <meta name="twitter:title" content={`${customHead.title} - ${Website.name}`} />
+                <meta name="twitter:description" content={customHead.description ?? ''} />
+                <meta name="twitter:image" content={customHead.image ?? ''} />
+                <meta name="twitter:card" content="website" />
             </Head>
         </>
     )
 }
+
+export default withRouter(HeadTitle)
