b5ee82a3fd4384e58c4ef5c42a6d4560ab7735e0
JuniYadi <juniyadi94@gmail.com>
2020-07-29 01:48:11 +0700

    Update HeadContent Component: Responsive and Resize Image

--- components/HeadContent.jsx
+++ components/HeadContent.jsx
@@ -4,17 +4,18 @@ export default function HeadContent(props) {
         <>
             <div className="pt-16">
                 <div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
-                    <div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
-                        <h1 className="my-4 text-2xl md:text-4xl font-bold leading-tight">
-                            {props.title}
-                        </h1>
-                        <p className="leading-normal text-xl md:text-2xl mb-8">{props.description}</p>
+                    <div className="flex flex-wrap flex-col-reverse sm:flex-row justify-center items-start">
+                        <div className="w-full sm:w-1/2 py-6 text-center md:text-left">
+                            <h1 className="my-4 text-2xl md:text-4xl font-bold leading-tight">
+                                {props.title}
+                            </h1>
+                            <p className="leading-normal text-xl md:text-2xl mb-8">{props.description}</p>
 
-                        {props.children}
-
-                    </div>
-                    <div className="w-full md:w-3/5 py-6 text-right">
-                        <img className="w-full md:w-4/5 z-50" src={props.imageSvg} />
+                            {props.children}
+                        </div>
+                        <div className="w-full sm:w-1/2 py-6 text-center md:text-right">
+                            <img className="w-full sm:h-64 mx-auto" src={props.imageSvg} />
+                        </div>
                     </div>
                 </div>
             </div>
