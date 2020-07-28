e9ea70c61abfb3a00994954e618d72241018a271
JuniYadi <juniyadi94@gmail.com>
2020-07-28 09:42:15 +0700

    Update Website Config Social Media

--- config/Website.jsx
+++ config/Website.jsx
@@ -4,4 +4,18 @@ export const Website = {
     email: 'cs@premiumfast.net',
     phone: '+62857 999-02074',
     whatsapp: '6285799902074',
+    social: {
+        facebook: {
+            name: 'Premium Fast Network',
+            url: 'https://web.facebook.com/premiumfastnetwork/'
+        },
+        twitter: {
+            name: 'PremiumFastNet',
+            url: 'https://twitter.com/PremiumFastNet'
+        },
+        youtube: {
+            name: 'Premium Fast Network',
+            url: 'https://www.youtube.com/channel/UC6hjIWnOlHsIMjcU10eRHVw'
+        }
+    }
 }
