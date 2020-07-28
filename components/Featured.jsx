7dce95aab266c69b2849b57d0b786d61365c517f
JuniYadi <juniyadi94@gmail.com>
2020-07-28 10:30:59 +0700

    Update Featured Components: `class` to `className` and Key Index

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -10,20 +10,20 @@ export default function Featured(props) {
     const lists = props.lists
 
     return (
-        <section class="bg-white border-b py-8">
-            <div class="container mx-auto px-2 pt-4 pb-12 text-gray-800">
-                <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+        <section className="bg-white border-b py-8">
+            <div className="container mx-auto px-2 pt-4 pb-12 text-gray-800">
+                <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
                     Features
                 </h1>
-                <div class="w-full mb-4">
-                    <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+                <div className="w-full mb-4">
+                    <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
                 </div>
 
-                <div class="flex flex-wrap">
+                <div className="flex flex-wrap">
 
                     {
-                        lists.map(list => (
-                            <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 mb-4">
+                        lists.map((list, index) => (
+                            <div key={`featurelist-${index}`} id={`featurelist-${index}`} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 mb-4">
                                 <div className="theme-font-color text-center text-sm">
                                     <div className="text-5xl">
                                         <FontAwesomeIcon icon={list.icon} />
