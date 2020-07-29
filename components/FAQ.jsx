c35ee580e0b75f5209770513fb9cda2c0d42e5bb
JuniYadi <juniyadi94@gmail.com>
2020-07-29 12:57:00 +0700

    Added FAQ Component

---
+++ components/FAQ.jsx
@@ -0,0 +1,44 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import React from 'react'
+
+export default function FAQ(props) {
+    const lists = props.lists
+
+    const addLineBreaks = (string) => {
+        return string.split('\n').map((text, index) => (
+            <React.Fragment key={`br-${index}`}>
+            {text}
+            <br />
+            </React.Fragment>
+        ));
+    }
+
+    return (
+        <section className="bg-white border-b py-8">
+            <div className="container mx-auto px-2 pt-4 pb-12 text-gray-800">
+                <h1 className="w-full my-2 text-2xl lg:text-4xl font-bold leading-tight text-center text-gray-800">
+                    FAQ Product
+                </h1>
+                <div className="w-full mb-4">
+                    <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+                </div>
+
+                <div className="flex flex-wrap">
+                    {
+                        lists.map((list, index) => (
+                            <div key={`faq-${index}`} id={`faq-${index}`} className="w-full sm:w-1/1 md:w-1/2 lg:w-1/2 xl:w-1/2 rounded group">
+                                <h3 className="bg-gray-700 text-white text-base lg:text-xl font-bold rounded my-2 mx-2 py-2 px-2">   
+                                    {list.title} <FontAwesomeIcon icon={faArrowDown} />
+                                </h3>
+                                <div className="bg-gray-700 text-white text-justify hidden rounded group-hover:block mx-2 my-2">
+                                    <p className="py-2 px-2 text-sm lg:text-base">{addLineBreaks(list.description)}</p>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </section>
+    )
+}
