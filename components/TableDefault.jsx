e645f80e59aada017e078e97736dcf82a5210fc0
JuniYadi <juniyadi94@gmail.com>
2020-07-29 18:43:39 +0700

    Update TableDefault Component: Multiple Buy and Change Price Location

--- components/TableDefault.jsx
+++ components/TableDefault.jsx
@@ -5,6 +5,26 @@ export default function TableDefault(props) {
     const title = props.title ?? 'Harga Langganan'
     const products = props.products
 
+    // Check if Link is Array or Not
+    function isArrayOrder(data) {
+        if(Array.isArray(data)) {
+            return (
+                data.map((link, index) => (
+                    <a href={link.href} className="mx-auto lg:mx-0 hover:bg-green-800 bg-green-500 text-white font-bold rounded-full my-1 py-2 px-2 shadow-lg">
+                        <FontAwesomeIcon icon={faCartPlus} /> {link.title ?? 'Buy Now'}
+                    </a>
+                    
+                ))
+            )
+        } else {
+            return (
+                <a href={data} className="mx-auto lg:mx-0 hover:bg-green-800 bg-green-500 text-white font-bold rounded-full my-2 py-2 px-2 shadow-lg">
+                    <FontAwesomeIcon icon={faCartPlus} /> Buy Now
+                </a>
+            )
+        }
+    }
+
     return (
         <section className="bg-gray-100 py-8">
             <div className="container mx-auto px-2 pt-4 pb-12 text-gray-800">
@@ -21,8 +41,13 @@ export default function TableDefault(props) {
                             return (
                                 <div key={`product-${index}`} id={`product-${index}`} className={`flex flex-col w-5/6 mx-auto lg:mx-0 ${product.featured == true ? 'lg:w-1/4 rounded-lg bg-white mt-4 sm:-mt-6 shadow-lg z-10' : 'lg:w-1/4 rounded-none lg:rounded-l-lg bg-white mt-4'}`}>
                                     <div className={`flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow ${product.featured == true ? '' : 'text-gray-700'}`}>
-                                        <div className={`${product.featured == true ? 'w-full p-4 text-3xl' : 'p-4 text-3xl border-b-4'} font-bold text-center`}>{product.name}</div>
+
+                                        <div className={`text-xl ${product.featured == true ? 'w-full md:text-3xl gradient text-white' : 'bg-gray-500 md:text-2xl'} shadow font-bold text-center`}>{product.name}</div>
+
+                                        <div className={`border-b-4 ${product.featured == true ? 'text-4xl font-bold text-center' : 'text-2xl text-gray-600 font-bold text-center'}`}>{product.price}<span className="text-base">{product.duration}</span></div>
+
                                         <div className={`${product.featured == true ? 'h-1 w-full gradient my-0 py-0 rounded-t' : ''} mb-2`}></div>
+
                                         <ul className={`w-full text-center ${product.featured == true ? 'text-base font-bold' : 'text-sm'}`}>
                                             {
                                                 product.description.map((desc, index) => (
@@ -31,14 +56,9 @@ export default function TableDefault(props) {
                                             }
                                         </ul>
                                     </div>
-                                    <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-4">
-                                        <div className={`w-full pt-2 ${product.featured == true ? 'text-4xl font-bold text-center' : 'text-2xl text-gray-600 font-bold text-center'}`}>
-                                            {product.price}<span className="text-base">{product.duration}</span>
-                                        </div>
-                                        <div className="flex items-center justify-center">
-                                            <a href={product.link} className="mx-auto lg:mx-0 hover:bg-green-800 bg-green-500 text-white font-bold rounded-full my-6 py-2 px-4 shadow-lg">
-                                                <FontAwesomeIcon icon={faCartPlus} /> Buy Now
-                                            </a>
+                                    <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow pb-4">
+                                        <div className="flex flex-col items-center justify-center">
+                                            {isArrayOrder(product.link)}
                                         </div>
                                     </div>
                                 </div>
