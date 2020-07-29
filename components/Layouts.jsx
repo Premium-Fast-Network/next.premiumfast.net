e0487e2f22dbbf01590442c3825f28ff06d1b011
JuniYadi <juniyadi94@gmail.com>
2020-07-29 13:58:37 +0700

    Update Layouts Component: Added TawkTo Chat Widget

--- components/Layouts.jsx
+++ components/Layouts.jsx
@@ -1,7 +1,42 @@
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import { useEffect } from 'react';
 
 export default function Layouts(props) {
+    function tawkTo(propertyId){
+
+        if (!window) {
+            throw new Error('DOM is unavailable')
+        }
+    
+        window.Tawk_API       = window.Tawk_API || {};
+        window.Tawk_LoadStart = new Date();
+    
+        const tawk = document.getElementById('tawkId');
+        if (tawk) {
+            // Prevent TawkTo to create root script if it already exists
+            return window.Tawk_API;
+        }
+    
+        const script = document.createElement("script");
+        script.id    = 'tawkId';
+        script.async = true;
+        script.src   = 'https://embed.tawk.to/' + propertyId + '/default';
+        script.charset = 'UTF-8';
+        script.setAttribute('crossorigin', '*');
+    
+        const first_script_tag = document.getElementsByTagName("script")[0];
+        if (!first_script_tag || !first_script_tag.parentNode) {
+            throw new Error('DOM is unavailable')
+        }
+    
+        first_script_tag.parentNode.insertBefore(script, first_script_tag)
+    }
+
+    useEffect(() => {
+        tawkTo('565f1adc143d6d8f72be968d')
+    }, [])
+
     return (
         <>
             <Navbar />
