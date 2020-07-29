39d31b4b0f3928444d1de1b0db8e923381b81640
JuniYadi <juniyadi94@gmail.com>
2020-07-29 11:24:59 +0700

    Update Pages AboutUs: Resize Font in Mobile

--- pages/about-us.jsx
+++ pages/about-us.jsx
@@ -28,8 +28,8 @@ export default function Home() {
         </HeadContent>
 
         <section className="bg-white border-b py-8">
-          <div className="container m-8">
-            <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+          <div className="container mx-auto px-2 pt-4 pb-12 text-gray-800">
+            <h1 className="w-full my-2 text-2xl lg:text-4xl font-bold leading-tight text-center text-gray-800">
                 Tentang Kami
             </h1>
             <div className="w-full mb-4">
@@ -78,7 +78,7 @@ export default function Home() {
               </ul>
             </div>
 
-            <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
+            <h1 className="w-full my-2 text-2xl lg:text-4xl font-bold leading-tight text-center text-gray-800">
                 Timeline
             </h1>
             <div className="w-full mb-4">
